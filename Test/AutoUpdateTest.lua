862900d68724cd52e756b153285b4246488b72c7
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-10-14 15:21:08 -0400

    Delete AutoUpdateTest.lua

--- Test/AutoUpdateTest.lua
+++
@@ -1,8 +0,0 @@
-import "UnityEngine"
-
-meta["source"] = "https://raw.githubusercontent.com/Metalloriff/ElementalReignMods/Test/AutoUpdateTest.lua"
-meta["version"] = "0.0.2"
-
-function Load()
-	Debug.Log(meta["version"])
-end
