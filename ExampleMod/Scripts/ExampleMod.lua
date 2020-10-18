4e69cd82a13cefb71fc7598c37f28429d69f0ada
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-10-18 12:49:08 -0400

    Update ExampleMod.lua

--- ExampleMod/Scripts/ExampleMod.lua
+++ ExampleMod/Scripts/ExampleMod.lua
@@ -8,10 +8,16 @@ import ("PhotonUnityNetworking", "Photon.Pun") -- Imports the networking system 
 meta["source"] = "https://raw.githubusercontent.com/Metalloriff/ElementalReignMods/main/ExampleMod/Scripts/ExampleMod.lua"
 	-- The source meta is used to request the updated version of the script, this is required for auto-updates to work.
 	-- NOTE: The source meta, if present, must be paired with the version meta.
-meta["version"] = "0.0.1"
+meta["version"] = "0.0.2"
 	-- The version meta is used for update version comparisons.
 	-- If the remote source version is higher than the local version, an update request prompt will be shown.
 	-- NOTE: The version must follow the semantic versioning format (https://semver.org/) of MAJOR.MINOR.PATCH.
+meta["description"] = "An example mod showcasing most of the API available for script modding."
+	-- The description meta is used to describe what your script/mod does. This will show in the game launcher/mod manager.
+-- meta["required"] = "some mod, some other mod"
+	-- The required meta is used to require other mods. This can be scripts or assets.
+	-- This will ensure that your script will not start unless all required mods are installed.
+	-- NOTE: This is commented out to ensure that the mod will still start without the fake requirement examples.
 
 function Load() -- Called as soon as the script starts. Use this for initialization.
 	test = GameObject.Find("Test")
@@ -24,6 +30,25 @@ function Load() -- Called as soon as the script starts. Use this for initializat
 	-- your commands. If you register a command, you must unregister it with API:UnregisterAllCommands() before re-registering. (recommended in Unload())
 	API:RegisterCommand(Console.Command("size", "Scales the player.", "size 5",
 		Console.Command.Argument("size multiplier", "The amount of times by default to scale the player to.")))
+
+	-- Two example settings of different types. You can have an infinite number of settings fields.
+	-- NOTE: The settings types must be serializable.
+	someIntSetting = 5
+	someStringSetting = "test"
+
+	-- Load the settings fields from the settings.json file for this mod.
+	API:LoadSettings(Name, "someIntSetting", "someStringSetting")
+end
+
+-- Call this function to see how applying and saving settings works.
+function ExampleSaveSettings()
+	-- Example of setting the setting fields.
+	-- The integer is set to a random number between 0 and 19, and the string is set to a randomly generated string with a length of 5.
+	someIntSetting = Random.Range(0, 20)
+	someStringSetting = Lib.CreateUniqueID(5)
+
+	-- Now save the settings, the same as you do when you load them.
+	API:SaveSettings(Name, "someIntSetting", "someStringSetting")
 end
 
 function Unload() -- Called before the script is unloaded (usually when reloading). Use this to detach events and objects created during your script's runtime.
